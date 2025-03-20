09b10b87420ef0da1f34c5a438329eea29f0345e
Yashgoswami14 <goswamiyash256@gmail.com>
2025-03-20 18:36:33 +0530

    design payment and analytics controllers and backend is complete

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js"
 import couponRoutes from "./routes/coupon.routes.js"
+import paymentRoutes from "./routes/payment.routes.js"
+import analyticsRoutes from "./routes/analytics.routes.js"
 
 import { connectDB } from "./lib/db.js";
 
@@ -23,6 +25,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
 app.listen(PORT, () => {
   console.log(`app listening on port number ${PORT}`);
